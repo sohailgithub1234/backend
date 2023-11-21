7931f1622eaa1bb171fae016304ffbf5d03b7813
Sohail Ali Khan <microsohail@gmail.com>
2023-11-21 10:31:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Login in</h1>')
+  })
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
